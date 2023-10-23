f0e05f5c7edaeb7e017d302cdd15bf023fcd4651
AMAN SAHIL <as21mmb0a07@student.nitw.ac.in>
2023-10-23 17:22:22 +0530

    internet branch added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log("aman")
 console.log("Learning Git");
 console.log("Not able to understand anything")
+console.log("hey there ")
+setTimeout(function() {
+    console.log("I am learning git version control systems");
+}, 3000);
